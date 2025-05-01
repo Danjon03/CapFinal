db1cfdbb1a566d9024cd0eebc24bf0a2716002c3
mirandaaarm <160691500+mirandaaarm@users.noreply.github.com>
2025-05-01 12:59:10 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // server.js
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 
 const { ObjectId } = require('mongodb');
@@ -19,6 +21,21 @@ const init = async () => {
 
         }
     });
+
+    await server.register(Inert);
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: Path.join(__dirname, 'capstone'), // or 'frontend' or wherever your HTML files live
+                listing: false,
+                index: true
+            }
+        }
+    });
+
 
     // MongoDB connection setup
     const client = new MongoClient('mongodb://localhost:27017');
